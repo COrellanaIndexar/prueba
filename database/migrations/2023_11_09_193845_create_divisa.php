64b40efa757c357b5be915c925d35ea6ef1cca8c
COrellanaIndexar <corellana@indexar.cl>
2023-12-02 23:49:16 -0300

    divisa not-insertion

--- database/migrations/2023_11_09_193845_create_divisa.php
+++ database/migrations/2023_11_09_193845_create_divisa.php
@@ -23,26 +23,26 @@ return new class extends Migration
         });
 
         //insercion de datos
-        Divisa::insert([
-          ['nombre' => 'Peso Argentino', 'codigo' => 'ARS', 'simbolo' => '$'],
-          ['nombre' => 'Boliviano', 'codigo' => 'BOB', 'simbolo' => 'Bs.'],
-          ['nombre' => 'Peso Chileno', 'codigo' => 'CLP', 'simbolo' => '$'],
-          ['nombre' => 'Peso Colombiano', 'codigo' => 'COP', 'simbolo' => '$'],
-          ['nombre' => 'Colón Costarricense', 'codigo' => 'CRC', 'simbolo' => '?'],
-          ['nombre' => 'Peso Cubano', 'codigo' => 'CUP', 'simbolo' => '$'],
-          ['nombre' => 'Dólar Estadounidense', 'codigo' => 'USD', 'simbolo' => '$'],
-          ['nombre' => 'Quetzal', 'codigo' => 'GTQ', 'simbolo' => 'Q'],
-          ['nombre' => 'Lempira', 'codigo' => 'HNL', 'simbolo' => 'L'],
-          ['nombre' => 'Peso Mexicano', 'codigo' => 'MXN', 'simbolo' => '$'],
-          ['nombre' => 'Córdoba', 'codigo' => 'NIO', 'simbolo' => 'C$'],
-          ['nombre' => 'Balboa', 'codigo' => 'PAB', 'simbolo' => 'B/ o $'],
-          ['nombre' => 'Guaraní Paraguayo', 'codigo' => 'PYG', 'simbolo' => '?'],
-          ['nombre' => 'Sol Peruano', 'codigo' => 'PEN', 'simbolo' => 'S/ o S/.'],
-          ['nombre' => 'Peso Dominicano', 'codigo' => 'DOP', 'simbolo' => 'RD$'],
-          ['nombre' => 'Peso Uruguayo', 'codigo' => 'UYU', 'simbolo' => '$U'],
-          ['nombre' => 'Bolívar Soberano', 'codigo' => 'VES', 'simbolo' => 'Bs. S.'],
-          ['nombre' => 'Real Brasileño', 'codigo' => 'BRL', 'simbolo' => 'R$'],
-      ]);
+      //   Divisa::insert([
+      //     ['nombre' => 'Peso Argentino', 'codigo' => 'ARS', 'simbolo' => '$'],
+      //     ['nombre' => 'Boliviano', 'codigo' => 'BOB', 'simbolo' => 'Bs.'],
+      //     ['nombre' => 'Peso Chileno', 'codigo' => 'CLP', 'simbolo' => '$'],
+      //     ['nombre' => 'Peso Colombiano', 'codigo' => 'COP', 'simbolo' => '$'],
+      //     ['nombre' => 'Colón Costarricense', 'codigo' => 'CRC', 'simbolo' => '?'],
+      //     ['nombre' => 'Peso Cubano', 'codigo' => 'CUP', 'simbolo' => '$'],
+      //     ['nombre' => 'Dólar Estadounidense', 'codigo' => 'USD', 'simbolo' => '$'],
+      //     ['nombre' => 'Quetzal', 'codigo' => 'GTQ', 'simbolo' => 'Q'],
+      //     ['nombre' => 'Lempira', 'codigo' => 'HNL', 'simbolo' => 'L'],
+      //     ['nombre' => 'Peso Mexicano', 'codigo' => 'MXN', 'simbolo' => '$'],
+      //     ['nombre' => 'Córdoba', 'codigo' => 'NIO', 'simbolo' => 'C$'],
+      //     ['nombre' => 'Balboa', 'codigo' => 'PAB', 'simbolo' => 'B/ o $'],
+      //     ['nombre' => 'Guaraní Paraguayo', 'codigo' => 'PYG', 'simbolo' => '?'],
+      //     ['nombre' => 'Sol Peruano', 'codigo' => 'PEN', 'simbolo' => 'S/ o S/.'],
+      //     ['nombre' => 'Peso Dominicano', 'codigo' => 'DOP', 'simbolo' => 'RD$'],
+      //     ['nombre' => 'Peso Uruguayo', 'codigo' => 'UYU', 'simbolo' => '$U'],
+      //     ['nombre' => 'Bolívar Soberano', 'codigo' => 'VES', 'simbolo' => 'Bs. S.'],
+      //     ['nombre' => 'Real Brasileño', 'codigo' => 'BRL', 'simbolo' => 'R$'],
+      // ]);
     }
 
     /**
